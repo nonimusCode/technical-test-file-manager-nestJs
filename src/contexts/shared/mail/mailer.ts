8d7907984562deaf42bec23761f3917c2d511039
NonimusCode <nonimus.code@gmail.com>
2025-02-23 02:57:30 -0500

    feat: implement forgot password functionality with email validation and token management; enhance password reset process

--- src/contexts/shared/mail/mailer.ts
+++ src/contexts/shared/mail/mailer.ts
@@ -8,7 +8,9 @@ export class Mailer {
 
   constructor(private readonly configService: ConfigService) {
     this.transporter = nodemailer.createTransport({
-      service: "Gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: this.configService.get<string>("EMAIL_USER"),
         pass: this.configService.get<string>("EMAIL_PASS"),
