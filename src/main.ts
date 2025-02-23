9cecb8b5bea97973306f19219264f4349f5435a2
NonimusCode <nonimus.code@gmail.com>
2025-02-22 23:42:41 -0500

    feat: add swagger documentation and validation for authentication dtos

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // configure for
+  const config = new DocumentBuilder()
+    .setTitle("File Manager API")
+    .setDescription("API para la gestión de archivos")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().then(
