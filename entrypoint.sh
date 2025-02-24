0970dd003ef0fbd264c884a064be80a8b3741210
NonimusCode <nonimus.code@gmail.com>
2025-02-24 13:45:20 -0500

    refactor: streamline dockerfile and entrypoint for improved development setup update package dependencies

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
+#!/bin/sh
 set -e
 
-echo "Running Prisma migrations..."
+echo "🏃 Running project"
+
+echo "Running database migrations..."
 npx prisma migrate dev
+
+echo "🔧 Development mode"
 
 echo "Starting development server..."
 exec npm run dev
